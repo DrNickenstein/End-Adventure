22767df4bfe93f1f1e51530906b18e3efd0081a1
DrNickenstein <nicola-nicotera@libero.it>
2023-01-23 15:22:13 +0100

    Added the sword tab

--- src/main/java/io/github/drnickenstein/endadventure/events/EACreativeModeTabs.java
+++ src/main/java/io/github/drnickenstein/endadventure/events/EACreativeModeTabs.java
@@ -6,16 +6,19 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.registries.RegistryObject;
 
 public class EACreativeModeTabs {
 	
 	public static CreativeModeTab MISC_EA_TAB;
+	public static CreativeModeTab EA_SWORDS_TAB;
 	
 	public static void registerTabs(final CreativeModeTabEvent.Register e) {
 
 		MISC_EA_TAB = register(e, new ResourceLocation(EndAdventure.MODID, "misc"));
+		EA_SWORDS_TAB = registerSwordsTab(e, new ResourceLocation(EndAdventure.MODID, "swords"));
 		
 	}
 	
@@ -29,6 +32,28 @@ public class EACreativeModeTabs {
 					for(final RegistryObject<Item> item : ItemInit.ITEMS.getEntries()) {
 						
 						displayedItems.accept(item.get());
+						
+					}
+					
+				}				
+		));
+		
+	}
+	
+	private static CreativeModeTab registerSwordsTab(CreativeModeTabEvent.Register e, ResourceLocation resourceLocation) {
+
+		return e.registerCreativeModeTab(resourceLocation, builder -> builder
+				.icon(() -> ItemInit.FINISIUM_SWORD.get().getDefaultInstance())
+				.title(Component.literal("End Adventure Swords"))
+				.displayItems((featureFlagSet, displayedItems, hasPermissions) -> {
+					
+					for(final RegistryObject<Item> item : ItemInit.ITEMS.getEntries()) {
+						
+						if(item.get() instanceof SwordItem) {
+							
+							displayedItems.accept(item.get());
+						
+						}
 						
 					}
 					
